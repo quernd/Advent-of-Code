b00c32c7252d2ecef99cd92af6fabf362cae14b5
Daniel Quernheim <quernd@users.noreply.github.com>
2023-12-03 12:57:31 +0000

    More utility functions

--- 2023/util/util.ml
+++ 2023/util/util.ml
@@ -95,3 +95,41 @@ let%expect_test "zip" =
   [%expect {| [[1; 4; 7]; [2; 5; 8]; [3; 6; 9]] |}]
 
 let explode s = String.to_seq s |> List.of_seq
+
+module P = struct
+  let is_digit = function '0' .. '9' -> true | _ -> false
+end
+
+module Coordinate = struct
+  type t = int * int [@@deriving show]
+
+  let adj (x, y) = [ (x - 1, y); (x + 1, y); (x, y - 1); (x, y + 1) ]
+
+  let adj_diag (x, y) =
+    [ (x - 1, y - 1); (x - 1, y + 1); (x + 1, y - 1); (x + 1, y + 1) ]
+
+  let adj' (x, y) = adj (x, y) @ adj_diag (x, y)
+  let compare = compare
+  let ( + ) (x, y) (x', y') = (x + x', y + y')
+end
+
+module Cmap = struct
+  include Map.Make (Coordinate)
+
+  let pp pp' fmt =
+    iter (fun key value ->
+        Format.fprintf fmt "%a -> %a\n" Coordinate.pp key pp' value)
+end
+
+module Cset = struct
+  include Set.Make (Coordinate)
+
+  let pp fmt set =
+    Format.pp_print_string fmt "{";
+    Format.pp_print_seq
+      ~pp_sep:(fun fmt () -> Format.pp_print_string fmt ", ")
+      Coordinate.pp fmt (to_seq set)
+end
+
+let sum = List.fold_left ( + ) 0
+let product = List.fold_left ( * ) 1
